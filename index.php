64ce57660543894c40ef3ffefed8c1c406125608
AmalAntoney123 <amalantoney2025@mca.ajce.in>
2023-02-22 10:42:31 +0530

    commit 1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>New Tittle - House services</title>
+    <title>Pro Homes - House services</title>
 
     <link rel="icon" href="assets/imgs/Logo.png" type="image/icon type">
 
