3ff514984eefb44ca415347c0f055926bbbb4fb6
AmalAntoney123 <amalantoney2025@mca.ajce.in>
2023-02-22 10:39:47 +0530

    Revert "changed title"
    This reverts commit d7175643ae16b1b2dfa6dd4431e52f4921ad62ac.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>ahsdab - House services</title>
+    <title>New Tittle - House services</title>
 
     <link rel="icon" href="assets/imgs/Logo.png" type="image/icon type">
 
